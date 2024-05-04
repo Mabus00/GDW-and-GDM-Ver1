6e3ee6261928642e316bf964a221889572767c30
Mabus00 <wayne.renaud@gmail.com>
2024-05-04 14:53:52 -0300

    went back to tomcat

--- scripts/parse_3D_giops_dataset.py
+++ scripts/parse_3D_giops_dataset.py
@@ -1,8 +1,12 @@
 from netCDF4 import Dataset
+import xarray as xr
 import os
 
+# Get the current working directory
+cwd = os.getcwd()
+
 # Path to the folder containing NetCDF files
-folder_path = "C:/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/geoserver/data/giops_3D_data"
+folder_path = 'C:/Program Files/Apache Software Foundation/Tomcat 9.0/webapps/geoserver/data/giops_3D_data'
 
 # Get a list of all filenames in the folder
 file_names = os.listdir(folder_path)
@@ -18,42 +22,37 @@ for file_name in file_names:
     file_path = file_path.replace('\\', '/')
 
     # Open the NetCDF file
-    nc_file = Dataset(file_path, "r")
+    ds = xr.open_dataset(file_path)
+
+    ''' 
+    Dimensions:             (longitude: 1800, latitude: 850, depth: 50, time: 1)
+    Coordinates:
+        * longitude           (longitude) float32 0.0 0.2 0.4 ... 359.4 359.6 359.8
+        * latitude            (latitude) float32 -80.0 -79.8 -79.6 ... 89.4 89.6 89.8
+        * depth               (depth) float32 0.494 1.541 ... 5.275e+03 5.728e+03
+        * time                (time) datetime64[ns] 2024-05-05T12:00:00
+        Data variables:
+            latitude_longitude  int32 0
+            votemper            (time, depth, latitude, longitude) float32 nan ... nan
+        Attributes:
+            Conventions:      CF-1.6
+            title:            Instantaneous ice and ocean forecast fields
+            institution:      The Canadian Centre for Meteorological and Environmenta...
+            source:           Global Ice Ocean Prediction System: version 8.1.0
+            product_version:  GIOPS_8.1.0_F3
+            creation_date:    2024-May-04 16:29:52 UTC
+            contact:          production-info@ec.gc.ca
+
+    '''
+
+    print(ds.votemper)
 
     # Create a new folder based on the original file name
-    output_folder_name = file_name.split(".")[0]  # Remove the file extension
-    output_folder_path = os.path.join(folder_path, output_folder_name)
-    os.makedirs(output_folder_path, exist_ok=True)  # Create the folder if it doesn't exist
+    # output_folder_name = file_name.split(".")[0]  # Remove the file extension
+    # output_folder_path = os.path.join(folder_path, output_folder_name)
+    # os.makedirs(output_folder_path, exist_ok=True)  # Create the folder if it doesn't exist
 
-    # Extract the depth levels
-    depths = nc_file.variables["depth"][:]
+    # output_file_path = os.path.join(output_folder_path, f"{file_name}_{rounded_depth}_votemper.nc")
+    
+    print(f"File saved:")
 
-    # Iterate over each depth level
-    for i, depth in enumerate(depths):
-        # Extract data for this depth level
-        data = nc_file.variables["votemper"][0, i, :, :]  # Assuming time dimension is 0
-        # Note: Change [0, i, :, :] indexing if time dimension is different
-        rounded_depth = [round(depth, 2) for depth in depths]
-        # Save data to a new netCDF file in the created folder
-        output_file_path = os.path.join(output_folder_path, f"{file_name}_{rounded_depth}_votemper.nc")
-        with Dataset(output_file_path, "w") as output_nc_file:
-            # Create dimensions
-            output_nc_file.createDimension("latitude", data.shape[0])
-            output_nc_file.createDimension("longitude", data.shape[1])
-            
-            # Create variables
-            latitude_var = output_nc_file.createVariable("latitude", "f4", ("latitude",))
-            longitude_var = output_nc_file.createVariable("longitude", "f4", ("longitude",))
-            votemper_var = output_nc_file.createVariable("votemper", "f4", ("latitude", "longitude"))
-            
-            # Assign data and attributes
-            latitude_var[:] = nc_file.variables["latitude"][:]
-            longitude_var[:] = nc_file.variables["longitude"][:]
-            votemper_var[:, :] = data[:, :]
-            votemper_var.units = "Kelvin"
-            # Add more attributes if needed
-            
-        print(f"File saved: {output_file_path}")
-
-    # Close the NetCDF file
-    nc_file.close()
